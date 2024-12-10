6a099d58f6e43cadae939dafe120a7b2ee475036
mskar <mskarzynski8@gmail.com>
2024-12-10 17:37:08 +0100

    library project

--- blueprints/users.py
+++ blueprints/users.py
@@ -1,11 +1,14 @@
 from enum import unique
+
+from werkzeug.security import generate_password_hash
+
 from blueprints import db
 class Users(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), unique=True)
     password = db.Column(db.String(60), unique=True)
-    repeat_password = db.Column(db.String(60), unique=True)
-    def __init__(self,email,password,repeat_password):
+    answer = db.Column(db.String(60), unique=True)
+    def __init__(self,email,password,answer):
         self.email=email
         self.password=password
-        self.repeat_password=repeat_password
+        self.answer=answer
--- blueprints/welcome.py
+++ blueprints/welcome.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template, request, session
+from crypt import methods
+from logging import error
+
+from flask import Blueprint, render_template, request, session, url_for, flash
 from sqlalchemy.testing.suite.test_reflection import users
+from werkzeug.utils import redirect
 
 from .users import Users
 from . import db  # Import db from the app setup
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 login = Blueprint("login", __name__, template_folder="templates")
 register = Blueprint("register", __name__, template_folder="templates")
@@ -20,13 +24,12 @@ def login_users():
         email = request.form.get("email")
         password = request.form.get("password")
         found_user = Users.query.filter_by(email=email).first()
-        check_password=Users.query.filter_by(password=password).first()
         if not email or not password:
             return render_template("login.html", error="Pola nie mogą być puste")
         # Add login logic here, e.g., verify user credentials
         if not found_user:
             return render_template("login.html", error="Przykro mi taki użytkownik nie istnieje")
-        if password != found_user.password:
+        if not check_password_hash(found_user.password,password):
             return render_template("login.html", error="Podałeś nieprawidłowe hasło")
         session['email'] = found_user.email
         return render_template("homepage.html",succes = "Zalogowano pomyślnie")
@@ -39,18 +42,51 @@ def register_user():
         email = request.form.get('email')
         password = request.form.get('password')
         repeat_password = request.form.get('repeat_password')
+        answer = request.form.get("answer")
 
-        if not email or not password or not repeat_password:
+        if not email or not password or not repeat_password or not answer:
             return render_template("register.html", error="Masz gdzieś puste pola")
 
         if password != repeat_password:
             return render_template("register.html", error="Hasła muszą być takie same")
+        hash_answer = generate_password_hash(answer)
+        hashed_password = generate_password_hash(password)
+        with db.session.begin():# Securely hash the password
+            user = Users(email=email, password=hashed_password, answer=hash_answer)
 
-        hashed_password = generate_password_hash(password)  # Securely hash the password
-        user = Users(email=email, password=hashed_password, repeat_password=repeat_password)
-
-        db.session.add(user)
-        db.session.commit()
-        return render_template("register.html", success="Wprowadziłeś dane poprawnie")
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("login.index"))
 
     return render_template("register.html")
+@register.route("/change_password", methods=["POST","GET"])
+def change_password():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        repeat_password = request.form.get('repeat_password')
+        answer = request.form.get('answer')
+        found_user = Users.query.filter_by(email=email).first()
+        if not email or not password or not repeat_password or not answer:
+            flash("Wszystkie pola muszą być uzupełnione","error")
+            return render_template("change_password.html")
+        if password != repeat_password:
+            flash("Hasła się ze sobą nie zgadzają","error")
+            return render_template("change_password.html")
+        if not found_user:
+            flash("Nie ma takiego użytkownika o takiej nazwie")
+            return render_template("change_password.html")
+        if not check_password_hash(found_user.answer,answer):
+            flash("Podałeś złą odpowiedź", "error")
+            return render_template("change_password.html")
+        new_hashed_password =generate_password_hash(password)
+        found_user.password = new_hashed_password
+        try:
+            db.session.commit()
+            flash("Hało zostało zmienione","success")
+            return redirect(url_for("index"))
+        except Exception as e:
+            db.session.rollback()
+            flash("Coś poszło nie tak","error")
+            return render_template("change_password.html")
+    return render_template("change_password.html")
