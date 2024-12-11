8eb348bbe0e65976b92025ff6d6d40798620fc50
mskar <mskarzynski8@gmail.com>
2024-12-11 14:43:56 +0100

    library project

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 def create_blueprints():
-    from blueprints.welcome import login, register
+    from blueprints.welcome import login, register, log_out
     app = Flask(__name__, template_folder="/home/mskar/PycharmProjects/library/templates")
     app.secret_key = "superkey"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mskar/PycharmProjects/library/books.sqlite'
@@ -10,4 +10,5 @@ def create_blueprints():
     db.init_app(app)
     app.register_blueprint(login)  # Registers the login blueprint
     app.register_blueprint(register)
+    app.register_blueprint(log_out)
     return app
--- blueprints/welcome.py
+++ blueprints/welcome.py
@@ -1,7 +1,8 @@
 from crypt import methods
+from functools import wraps
 from logging import error
 
-from flask import Blueprint, render_template, request, session, url_for, flash
+from flask import Blueprint, render_template, request, session, url_for, flash, make_response
 from sqlalchemy.testing.suite.test_reflection import users
 from werkzeug.utils import redirect
 
@@ -11,12 +12,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 login = Blueprint("login", __name__, template_folder="templates")
 register = Blueprint("register", __name__, template_folder="templates")
-
+log_out = Blueprint("logout", __name__, template_folder="templates")
 
 @login.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("login.html")
 
 @login.route("/login", methods=["POST", "GET"])
 def login_users():
@@ -65,34 +65,55 @@ def register_user():
             return redirect(url_for("login.index"))
 
     return render_template("register.html")
-@register.route("/change_password", methods=["POST","GET"])
+
+
+@register.route("/change_password", methods=["POST", "GET"])
 def change_password():
+    # Jeśli nie, przekierowanie do logowania
+
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
         repeat_password = request.form.get('repeat_password')
         answer = request.form.get('answer')
-        found_user = Users.query.filter_by(email=email).first()
+
+        # Weryfikacja, czy wszystkie pola są wypełnione
         if not email or not password or not repeat_password or not answer:
-            flash("Wszystkie pola muszą być uzupełnione","error")
+            flash("Wszystkie pola muszą być uzupełnione", "error")
             return render_template("change_password.html")
+
+        # Weryfikacja, czy hasła się zgadzają
         if password != repeat_password:
-            flash("Hasła się ze sobą nie zgadzają","error")
+            flash("Hasła muszą się zgadzać", "error")
             return render_template("change_password.html")
+
+        # Szukamy użytkownika w bazie
+        found_user = Users.query.filter_by(email=email).first()
+
         if not found_user:
-            flash("Nie ma takiego użytkownika o takiej nazwie")
+            flash("Nie ma takiego użytkownika o takiej nazwie", "error")
             return render_template("change_password.html")
-        if not check_password_hash(found_user.answer,answer):
+
+        # Weryfikacja odpowiedzi na pytanie zabezpieczające
+        if not check_password_hash(found_user.answer, answer):
             flash("Podałeś złą odpowiedź", "error")
             return render_template("change_password.html")
-        new_hashed_password =generate_password_hash(password)
+
+        # Haszowanie nowego hasła
+        new_hashed_password = generate_password_hash(password)
         found_user.password = new_hashed_password
+
         try:
-            db.session.commit()
-            flash("Hało zostało zmienione","success")
-            return redirect(url_for("index"))
-        except Exception as e:
-            db.session.rollback()
-            flash("Coś poszło nie tak","error")
+            db.session.commit()  # Zapisz zmiany w bazie
+            flash("Hasło zostało zmienione", "success")
+            return redirect(url_for("index"))  # Po udanej zmianie hasła, przekierowanie na stronę główną
+        except Exception:
+            db.session.rollback()  # W przypadku błędu, wycofaj zmiany
+            flash("Coś poszło nie tak", "error")
             return render_template("change_password.html")
+
     return render_template("change_password.html")
+@log_out.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('login.login_users'))
